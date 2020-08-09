8a864d0aba7bb43aafc807d947860e07226ce3b6
zewas1 <matas.budriunas1@gmail.com>
2020-08-09 22:50:58 +0300

    Menu logic remake, code optimization, plan update.

--- src/main/java/menus/AdminMenu.java
+++ src/main/java/menus/AdminMenu.java
@@ -7,59 +7,33 @@ import java.sql.SQLException;
 
 public class AdminMenu {
 
-    private static int selectUser;
-    private static User user = new User();
-
-    // idea to self, make menu lead into services based on what functions i'm gonna add, because some services
-    // might be the same for multiple menus (e.g. check stats).
-
     public static void getAdminMenu() throws SQLException {
         do {
-            getMenuSelection();
+            UserMenu.getMenuSelection();
             if (MainClass.menuSelection == 1) {
-                showStats();
+                UserMenu.showStats();
             } else if (MainClass.menuSelection == 2) {
-                countKda();
+                UserMenu.countKda();
             } else if (MainClass.menuSelection == 3){
-                LoginSystem.getUsersFromDb(MainMenu.url, MainMenu.username, MainMenu.password);
-                System.out.println("Select user by ID");
-                for (User user : LoginSystem.userList) {
-                    System.out.println(user.getUserId()+ " " + user.getUserLogin());
-                }
-                selectUser = Integer.parseInt(MainClass.scan.next());
-                for (User user : LoginSystem.userList) {
-                    if (user.getUserId() == selectUser){
-                        System.out.println("User " + user.userLogin + " selected.");
-                    }
-                }
+                changeUserStats();
             }
         } while (MainClass.menuSelection != 4);
-        if (MainClass.menuSelection == 4)
-        {
-            LoginSystem.loginSuccessful = false;
-            LoginSystem.userList.clear();
-            LoginSystem.userListComparison.clear();
+        LoginSystem.loginSuccessful = false;
+        LoginSystem.userList.clear();
+        LoginSystem.userListComparison.clear();
+    }
+
+    private static void changeUserStats() throws SQLException {
+        LoginSystem.getUsersFromDb(MainMenu.url, MainMenu.username, MainMenu.password);
+        System.out.println("Select user by ID");
+        for (User user : LoginSystem.userList) {
+            System.out.println(user.getUserId()+ " " + user.getUserLogin());
         }
-    }
-
-    private static void countKda() {
-        System.out.println("KDA: " + (double) ((LoginSystem.currentAssists) / 2 + LoginSystem.currentKills) /
-                LoginSystem.currentDeaths);
-    }
-
-    private static void showStats() {
-        System.out.println("Kill count: " + LoginSystem.currentKills);
-        System.out.println("Death count: " + LoginSystem.currentDeaths);
-        System.out.println("Assist count: " + LoginSystem.currentAssists);
-    }
-
-    private static void getMenuSelection() {
-        System.out.println("Welcome, " + LoginSystem.currentUser);
-        System.out.println("This is a Admin menu. Available functions are:");
-        System.out.println("1. Check your stats");
-        System.out.println("2. Calculate KDA");
-        System.out.println("3. Upload user stats");
-        System.out.println("4. exit.");
-        MainClass.menuSelection = Integer.parseInt(MainClass.scan.next());
+        int selectUser = Integer.parseInt(MainClass.scan.next());
+        for (User user : LoginSystem.userList) {
+            if (user.getUserId() == selectUser){
+                System.out.println("User " + user.userLogin + " selected.");
+            }
+        }
     }
 }
--- src/main/java/menus/UserMenu.java
+++ src/main/java/menus/UserMenu.java
@@ -5,27 +5,36 @@ import main.MainClass;
 public class UserMenu {
     public static void getUserMenu() {
         do {
-            System.out.println("Welcome, " + LoginSystem.currentUser);
-            System.out.println("This is a user menu. Available functions are:");
-            System.out.println("1. Check your stats");
-            System.out.println("2. Calculate KDA");
-            //System.out.println("3. Play accuracy game.");
-            System.out.println("3. exit.");
-            MainClass.menuSelection = Integer.parseInt(MainClass.scan.next());
+            getMenuSelection();
             if (MainClass.menuSelection == 1) {
-                System.out.println("Kill count: " + LoginSystem.currentKills);
-                System.out.println("Death count: " + LoginSystem.currentDeaths);
-                System.out.println("Assist count: " + LoginSystem.currentAssists);
+                showStats();
             } else if (MainClass.menuSelection == 2) {
-                System.out.println("KDA: " + (double) ((LoginSystem.currentAssists) / 2 + LoginSystem.currentKills) /
-                        LoginSystem.currentDeaths);
+                countKda();
             }
         } while (MainClass.menuSelection != 3);
-        if (MainClass.menuSelection == 3)
-        {
-            LoginSystem.loginSuccessful = false;
-            LoginSystem.userList.clear();
-            LoginSystem.userListComparison.clear();
-        }
+        LoginSystem.loginSuccessful = false;
+        LoginSystem.userList.clear();
+        LoginSystem.userListComparison.clear();
+    }
+
+    static void countKda() {
+        System.out.println("KDA: " + (double) ((LoginSystem.currentAssists) / 2 + LoginSystem.currentKills) /
+                LoginSystem.currentDeaths);
+    }
+
+    static void showStats() {
+        System.out.println("Kill count: " + LoginSystem.currentKills);
+        System.out.println("Death count: " + LoginSystem.currentDeaths);
+        System.out.println("Assist count: " + LoginSystem.currentAssists);
+    }
+
+    static void getMenuSelection() {
+        System.out.println("Welcome, " + LoginSystem.currentUser);
+        System.out.println("This is a user menu. Available functions are:");
+        System.out.println("1. Check your stats");
+        System.out.println("2. Calculate KDA");
+        //System.out.println("3. Play accuracy game.");
+        System.out.println("3. exit.");
+        MainClass.menuSelection = Integer.parseInt(MainClass.scan.next());
     }
 }
